5cc9199b9ab78bf5900c5e2b3016e1a9a3844b75
FabyGithub7481 <gabogabidia@hotmail.com>
2022-09-21 22:23:38 -0500

    con autofocus 2

--- src/components/TaskCreator.js
+++ src/components/TaskCreator.js
@@ -3,11 +3,11 @@ import { useEffect, useRef, useState } from "react";
 export const TaskCreator = ({ createNewTask }) => {
   //   console.log(props);
   const [newTaskName, setNewTaskName] = useState("");
-  const inputNt = useRef(null)
+  const inputNt = useRef(null);
   useEffect(() => {
-  inputNt.current.focus()
-  }, [])
-  
+    inputNt.current.focus();
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     createNewTask(newTaskName);
@@ -15,19 +15,14 @@ export const TaskCreator = ({ createNewTask }) => {
     setNewTaskName("");
   };
 
-
-const FocusDemo = () => {
-
-  inputNt.current.focus()
-  
-  
-}
+  const FocusDemo = () => {
+    inputNt.current.focus();
+  };
 
   return (
     <form onSubmit={handleSubmit} className="my-2 row">
       <div className="col-9">
         <input
-          id="inputNt"
           type="text"
           placeholder="Enter a new task"
           value={newTaskName}
@@ -37,8 +32,9 @@ const FocusDemo = () => {
         />
       </div>
       <div className="col-3">
-        <button onClick={FocusDemo} className="btn btn-primary btn-sm">Save Task</button>
-      
+        <button onClick={FocusDemo} className="btn btn-primary btn-sm">
+          Save Task
+        </button>
       </div>
     </form>
   );
